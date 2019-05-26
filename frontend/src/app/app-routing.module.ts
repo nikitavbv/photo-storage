ab43c67e40ec4a526876cb4e0450203a0650f5e1
Nikita Volobuev <nikitavbv@gmail.com>
2019-05-26 15:06:32 +0300

    feat(frontend): routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingComponent} from "./landing";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+
+  // otherwise redirect to landing
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+
+import { HeaderComponent } from "./_shared/layout";
+import { AuthGuard } from "./_guard";
+import { AuthenticationService, PageTitleService } from "./_services";
+import { JwtInterceptor } from "./_helpers";
 import { AppComponent } from './app.component';
+import { LandingComponent } from "./landing";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+
+    LandingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AuthenticationService,
+    PageTitleService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
